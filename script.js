1d432b85835ac9bfc44521c1467a660b9b4504fa
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-13 22:04:01 +0530

    The exercise is complete and it plays the rock paper scissors
    playRound function plays a single round based on the choices
    tiePrevent function checks if the player choice and the computer choice is same if it happens to be same it agains generates the computer choice
    game function plays five round of the game and keeps a record of the wins of each player

--- script.js
+++ script.js
@@ -3,12 +3,73 @@
 function computerPlay() {
     var choice = Math.random();
     if(choice < 0.333) {
-        return 'Rock';
+        //console.log('rock');
+        return 'rock';
     } else if(choice >= .333 && choice < .666) {
-        return 'Paper';
+        //console.log('paper');
+        return 'paper';
     } else {
-        return 'Scissors';
+        //console.log('scissors');
+        return 'scissors';
     }
 }
 
+/*
+ P R S
+ Paper beats Rock
+ Rock beats Scissors
+ Scissors Beat Paper
+*/
 
+function playRound(playerSelection, computerSelection) {
+    if((playerSelection == 'paper' || playerSelection == 'scissors') && (computerSelection == 'paper' || computerSelection == 'scissors')) {
+        if(playerSelection < computerSelection) {
+            return `You! lose ${computerSelection} beats ${playerSelection}`;
+        } else {
+            return `You! win ${playerSelection} beats ${computerSelection}`;
+        }
+    } else {
+        if(playerSelection < computerSelection) {
+            return `You! win ${playerSelection} beats ${computerSelection}`;
+        } else {
+            return `You! lose ${computerSelection} beats ${playerSelection}`;
+        }
+    }
+}
+
+/*
+console.log(playRound('rock',computerPlay()));
+console.log(playRound('paper',computerPlay()));
+console.log(playRound('scissors',computerPlay()));
+console.log(playRound('paper',computerPlay()));
+console.log(playRound('rock',computerPlay()));
+*/
+
+function tiePrevent(playerChoice) {
+    var computerChoice = computerPlay(); 
+    while(true) {
+        if(playerChoice != computerChoice) {
+            return computerChoice;
+        }
+        computerChoice = computerPlay();
+    }
+}
+
+function game() {
+    var computerWinCount = 0, playerWinCount = 0;
+    var playerChoice, computerChoice, result;
+    for(var i = 0; i<5; i++) {
+        playerChoice = prompt('Enter your choice');
+        playerChoice = playerChoice.toLowerCase();
+        computerChoice = tiePrevent(playerChoice);
+        result = playRound(playerChoice, computerChoice)
+        console.log(result);
+        if (result.charAt(5) == 'w') {
+            playerWinCount += 1;
+        } else {
+            computerWinCount += 1;
+        }
+    }
+}
+
+game();
