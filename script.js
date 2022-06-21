2c668f730cfbccbef78510a10569689c3b5c8693
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-21 16:11:39 +0530

    Ui and the login elements are made Sorry for the less commit I am bad at everything

--- script.js
+++ script.js
@@ -62,20 +62,63 @@ function updateScore(playerWinCount, computerWinCount) {
     computerScore.textContent = computerWinCount + Number(computerScore.textContent);
 }
 
+function finishGame() {
+    const playerScore = document.querySelector('div.playerScore');
+    const computerScore = document.querySelector('div.computerScore');
+    const finalResult = document.querySelector('.finalResult');
+    if(playerScore.textContent === '5') {
+        finalResult.textContent = 'You win!';
+        disableButtons();
+        return true;
+    } else if(computerScore.textContent === '5') {
+        finalResult.textContent = 'You lose!';
+        disableButtons();
+        return true;
+    }
+}
+
+function disableButtons() {
+    const inputButtons = Array.from(document.querySelectorAll('input'));
+    inputButtons.forEach(button => button.disabled = true);
+}
+
+function playAgain() {
+    const playAgainButton = document.createElement('button');
+    playAgainButton.textContent = 'Play Again';
+    playAgainButton.addEventListener('click', function(){
+        window.location.reload();
+    })
+    const playAgain = document.querySelector('.playAgain');
+    playAgain.appendChild(playAgainButton);
+
+}
+
 function game(playerChoice) {
     let playerWinCount = 0, computerWinCount = 0;
     var computerChoice = tiePrevent(playerChoice);
-    var result = playRound(playerChoice, computerChoice)
+    var result = playRound(playerChoice, computerChoice);
+    const roundResult = document.querySelector('.roundResult');
+    roundResult.textContent = result;
     console.log(result);
     if (result.charAt(5) == 'w') {
         playerWinCount += 1;
+        if(roundResult.classList.contains('lose')) {
+            roundResult.classList.remove('lose');
+        }
+        roundResult.classList.add('win')
     } else {
+        if(roundResult.classList.contains('win')) {
+            roundResult.classList.remove('win');
+        }
+        roundResult.classList.add('lose')
         computerWinCount += 1;
     }
     updateScore(playerWinCount, computerWinCount);
+    var isEnded = finishGame();
+    if(isEnded) {
+        playAgain();
+    }
 }
-
-
 
 const inputButtons = Array.from(document.querySelectorAll('input'));
 inputButtons.forEach(button => button.addEventListener('click', function(){
